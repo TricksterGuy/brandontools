ead5dd22b188b598344a0abc47db74bdb590a648
Brandon <tricksterguy@live.com>
2013-12-28 04:21:39 -0800

    Commit for 4bpp mode 0 export.  The worst...

--- cli/reductionhelper.cpp
+++ cli/reductionhelper.cpp
@@ -5,12 +5,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <sstream>
+#include <unordered_set>
 
 #include "cpercep.hpp"
 #include "dither.hpp"
 #include "fileutils.hpp"
 #include "mediancut.hpp"
 #include "shared.hpp"
+
+uint32_t hashlittle( const void *key, size_t length, uint32_t initval);
 
 Image16Bpp::Image16Bpp(Magick::Image image, const std::string& _name) : width(image.columns()), height(image.rows()), name(_name), pixels(width * height)
 {
@@ -92,6 +95,11 @@ Palette::Palette(const std::vector<Color>& _colors, const std::string& _name) : 
         throw oss.str();
     }
 };
+
+void Palette::Set(const std::vector<Color>& _colors)
+{
+    colors = _colors;
+}
 
 int Palette::Search(const Color& a) const
 {
@@ -234,6 +242,188 @@ void Image8BppScene::WriteExport(std::ostream& file) const
         image.WriteExport(file);
 }
 
+PaletteBank::PaletteBank(const std::vector<Color>& _colors, int _id) : colors(_colors), id(_id)
+{
+    colorSet.insert(colors.begin(), colors.end());
+}
+
+bool PaletteBank::Contains(const Palette& palette) const
+{
+    for (const auto& color : palette.colors)
+    {
+        if (colorSet.find(color) == colorSet.end())
+            return false;
+    }
+    return true;
+}
+
+int PaletteBank::CanMerge(const Palette& palette) const
+{
+    std::set<Color> tmpset(colorSet);
+    for (const auto& color : palette.colors)
+        tmpset.insert(color);
+
+    return 16 - tmpset.size();
+}
+
+void PaletteBank::Merge(const Palette& palette)
+{
+    for (const auto& color : palette.colors)
+    {
+        if (colorSet.find(color) == colorSet.end())
+        {
+            colorSet.insert(color);
+            colors.push_back(color);
+        }
+    }
+}
+
+int PaletteBank::Search(const Color& a) const
+{
+    register double bestd = DBL_MAX;
+    int index = -1;
+
+    for (unsigned int i = 0; i < colors.size(); i++)
+    {
+        double dist = 0;
+        const Color& b = colors[i];
+        dist = a.Distance(b);
+        if (dist < bestd)
+        {
+            index = i;
+            bestd = dist;
+        }
+    }
+
+    if (bestd != 0)
+    {
+        printf("[WARNING] Color remap: Color given to palette bank not an exact match.\n");
+    }
+
+    return index;
+}
+
+template <>
+Tile<unsigned short>::Tile() : id(0), data(TILE_SIZE), bpp(16), palette_bank(0)
+{
+}
+
+template <>
+Tile<unsigned short>::Tile(const std::vector<unsigned short>& image, int pitch, int tilex, int tiley, int border, int ignored) : data(TILE_SIZE), palette_bank(0)
+{
+    Set(image, pitch, tilex, tiley, border, 16);
+}
+
+template <>
+Tile<unsigned short>::Tile(const unsigned short* image, int pitch, int tilex, int tiley, int border, int ignored) : data(TILE_SIZE), palette_bank(0)
+{
+    Set(image, pitch, tilex, tiley, border, 16);
+}
+
+template <>
+Tile<unsigned char>::Tile(std::shared_ptr<ImageTile> imageTile, int _bpp) : data(TILE_SIZE), bpp(_bpp)
+{
+    // bpp reduce
+    int num_colors = 1 << bpp;
+    const unsigned short* imgdata = imageTile->data.data();
+    int weights[4] = {25, 25, 25, 25};
+
+    std::vector<Color> paletteArray;
+    std::vector<Color> pixels(TILE_SIZE);
+
+    for (unsigned int i = 0; i < TILE_SIZE; i++)
+    {
+        unsigned short pix = imgdata[i];
+        pixels[i].Set(pix & 0x1f, (pix >> 5) & 0x1f, (pix >> 10) & 0x1f);
+    }
+
+    MedianCut(pixels, num_colors, paletteArray, weights);
+
+    palette.Set(paletteArray);
+
+    for (unsigned int i = 0; i < TILE_SIZE; i++)
+        data[i] = palette.Search(pixels[i]);
+
+    sourceTile = imageTile;
+}
+
+template <>
+void Tile<unsigned char>::UsePalette(const PaletteBank& bank)
+{
+    if (bpp == 8)
+    {
+        printf("Not implemented");
+        return;
+    }
+
+    std::map<int, int> remapping;
+    for (unsigned int i = 0; i < palette.Size(); i++)
+    {
+        Color old = palette.At(i);
+        remapping[i] = bank.Search(old);
+    }
+
+    for (unsigned int i = 0; i < TILE_SIZE; i++)
+    {
+        if (remapping.find(data[i]) == remapping.end())
+            throw "[FATAL] Somehow tile contains invalid indicies. This shouldn't happen";
+
+        data[i] = remapping[data[i]];
+    }
+
+    palette.Set(bank.colors);
+}
+
+template <>
+std::ostream& operator<<(std::ostream& file, const Tile<unsigned char>& tile)
+{
+    const std::vector<unsigned char>& data = tile.data;
+    if (tile.bpp == 8)
+    {
+        for (unsigned int i = 0; i < TILE_SIZE_SHORTS_8BPP; i++)
+        {
+            int px1 = data[2 * i];
+            int px2 = data[2 * i + 1];
+            unsigned short shrt = px1 | (px2 << 8);
+            WriteElement(file, shrt, TILE_SIZE_SHORTS_8BPP, i, 8);
+        }
+    }
+    else
+    {
+        for (unsigned int i = 0; i < TILE_SIZE_SHORTS_4BPP; i++)
+        {
+            int px1 = data[4 * i];
+            int px2 = data[4 * i + 1];
+            int px3 = data[4 * i + 2];
+            int px4 = data[4 * i + 3];
+            unsigned short shrt = (px4 << 12) | (px3 << 8) | (px2 << 4) | px1;
+            WriteElement(file, shrt, TILE_SIZE_SHORTS_4BPP, i, 8);
+        }
+    }
+    return file;
+}
+
+struct GBATileHash
+{
+    std::size_t operator()(const GBATile& k) const
+    {
+        return hashlittle(k.data.data(), TILE_SIZE, 0xDEADDEAD);
+    }
+};
+
+struct GBATile4bppEqual
+{
+    bool operator()(const GBATile& lhs, const GBATile& rhs) const
+    {
+        return lhs.data == rhs.data && lhs.palette_bank == rhs.palette_bank;
+    }
+};
+
+bool TilesPaletteSizeComp(const GBATile& i, const GBATile& j)
+{
+    return i.palette.Size() > j.palette.Size();
+}
+
 Tileset::Tileset(const std::vector<Image16Bpp>& images, const std::string& _name, int _bpp) : name(_name), bpp(_bpp)
 {
     switch(bpp)
@@ -286,6 +476,20 @@ int Tileset::Search(const ImageTile& tile) const
     return -1;
 }
 
+bool Tileset::Match(const ImageTile& tile, int& tile_id, int& pal_id) const
+{
+    const std::map<ImageTile, GBATile>::const_iterator foundTile = matcher.find(tile);
+    if (foundTile != matcher.end())
+    {
+        const GBATile& tile = foundTile->second;
+        tile_id = tile.id;
+        pal_id = tile.palette_bank;
+        return true;
+    }
+
+    return false;
+}
+
 void Tileset::WriteData(std::ostream& file) const
 {
     palette->WriteData(file);
@@ -315,48 +519,95 @@ void Tileset::WriteExport(std::ostream& file) const
 
 void Tileset::Init4bpp(const std::vector<Image16Bpp>& images)
 {
-
-}
-
-/*void GetTiles4bpp(std::vector<Magick::Image> images, const ExportParams& params, std::vector<GBATile>& tiles, std::vector<short>& offsets)
-{
-    // Form Image tiles
-    std::set<ImageTile> imageTiles;
-    GetTiles16bpp(images, params, imageTiles);
+    // Tile image into 16 bit tiles
+    Tileset tileset16bpp(images, name, 16);
+    std::set<ImageTile> imageTiles = tileset16bpp.itiles;
 
     // Reduce each tile to 4bpp
+    std::vector<GBATile> gbaTiles;
     for (std::set<ImageTile>::const_iterator i = imageTiles.begin(); i != imageTiles.end(); ++i)
-        tiles.push_back(GBATile(*i, 4));
+    {
+        std::shared_ptr<ImageTile> src(new ImageTile(*i));
+        GBATile tile(src, 4);
+        gbaTiles.push_back(tile);
+    }
 
     // Ensure image contains < 256 colors
     std::set<Color> bigPalette;
-    for (unsigned int i = 0; i < tiles.size(); i++)
+    for (const auto& tile : gbaTiles)
     {
-        const GBATile& tile = tiles[i];
-        const std::set<Color>& tile_palette = tile.GetPalette();
+        const std::vector<Color>& tile_palette = tile.palette.colors;
         bigPalette.insert(tile_palette.begin(), tile_palette.end());
     }
 
     if (bigPalette.size() > 256)
     {
-        // Ugh pain reform image from tiles and run 8bpp median cut
-        abort();
+        throw "[ERROR] Image after reducing tiles to 4bpp still contains more than 256 distinct colors. Please fix.";
     }
 
-    // merge palettes to form the palette
-    std::vector<PaletteBank> paletteBanks;
+    // Greedy approach deal with tiles with largest palettes first.
+    std::sort(gbaTiles.begin(), gbaTiles.end(), TilesPaletteSizeComp);
 
-    std::sort(tiles.begin(), tiles.end(), TilesPaletteSizeComp);
+    // Construct palette banks, assign bank id to tile, remap tile to palette bank given, assign tile ids
+    for (auto& tile : gbaTiles)
+    {
+        int pbank = -1;
+        // Fully contains checks
+        for (unsigned int i = 0; i < paletteBanks.size(); i++)
+        {
+            PaletteBank& bank = paletteBanks[i];
+            if (bank.Contains(tile.palette))
+                pbank = i;
+        }
 
+        // Ok then find least affected bank
+        if (pbank == -1)
+        {
+            int max_colors_left = -1;
+            for (unsigned int i = 0; i < paletteBanks.size(); i++)
+            {
+                PaletteBank& bank = paletteBanks[i];
+                int colors_left = bank.CanMerge(tile.palette);
+                if (colors_left != 0 && max_colors_left < colors_left)
+                {
+                    max_colors_left = colors_left;
+                    pbank = i;
+                }
+            }
+        }
 
-    // Checks
-    int tile_size = params.bpp == 8 ? TILE_SIZE_BYTES_8BPP : TILE_SIZE_BYTES_4BPP;
+        // Cry and die for now.
+        if (pbank == -1)
+            throw "More than 16 distinct palettes found, please use 8bpp mode.";
+
+        // Merge step and assign palette bank
+        paletteBanks[pbank].Merge(tile.palette);
+        tile.palette_bank = pbank;
+        tile.UsePalette(paletteBanks[pbank]);
+
+        // Assign tile id
+        std::set<GBATile>::const_iterator it = tiles.find(tile);
+        if (it == tiles.end())
+        {
+            tile.id = tiles.size();
+            tiles.insert(tile);
+        }
+        else
+        {
+            tile.id = it->id;
+        }
+
+        // Form mapping from ImageTile to GBATile
+        matcher[*tile.sourceTile] = tile;
+    }
+
+    int tile_size = TILE_SIZE_BYTES_4BPP;
     int memory_b = tiles.size() * tile_size;
     if (tiles.size() >= 1024)
     {
-        printf("[ERROR] Too many tiles found %zd tiles.\n"
-               "Please use -reduce or make the image simpler.\n", tiles.size());
-        exit(EXIT_FAILURE);
+        std::stringstream oss;
+        oss << "[ERROR] Too many tiles found" << tiles.size() << "tiles.\nPlease make the image simpler.\n";
+        throw oss.str();
     }
 
     // Delicious infos
@@ -366,8 +617,7 @@ void Tileset::Init4bpp(const std::vector<Image16Bpp>& images)
     printf("[INFO] Tiles uses %d charblocks and %d screenblocks.\n", cbbs, sbbs);
     printf("[INFO] Total utilization %.2f/4 charblocks or %d/32 screenblocks, %d/65536 bytes.\n",
            memory_b / ((double)SIZE_CBB_BYTES), (int) ceil(memory_b / ((double)SIZE_SBB_BYTES)), memory_b);
-
-}*/
+}
 
 void Tileset::Init8bpp(const std::vector<Image16Bpp>& images16)
 {
@@ -411,7 +661,7 @@ void Tileset::Init8bpp(const std::vector<Image16Bpp>& images16)
     }
 
     // Checks
-    int tile_size = params.bpp == 8 ? TILE_SIZE_BYTES_8BPP : TILE_SIZE_BYTES_4BPP;
+    int tile_size = TILE_SIZE_BYTES_8BPP;
     int memory_b = tiles.size() * tile_size;
     if (tiles.size() >= 1024)
     {
@@ -494,9 +744,27 @@ void Map::Set(const Image16Bpp& image, std::shared_ptr<Tileset> global_tileset)
             Init8bpp(image);
     }
 }
+
 void Map::Init4bpp(const Image16Bpp& image)
 {
+    const std::vector<unsigned short>& pixels = image.pixels;
 
+    for (unsigned int i = 0; i < data.size(); i++)
+    {
+        int tilex = i % width;
+        int tiley = i / width;
+        ImageTile imageTile(pixels, image.width, tilex, tiley);
+        int tile_id = 0;
+        int pal_id = 0;
+
+        if (!tileset->Match(imageTile, tile_id, pal_id))
+        {
+            printf("[WARNING] Image: %s No match for tile starting at (%d %d) px, using empty tile instead.\n", image.name.c_str(), tilex * 8, tiley * 8);
+            printf("[WARNING] Image: %s No match for palette for tile starting at (%d %d) px, using palette 0 instead.\n", image.name.c_str(), tilex * 8, tiley * 8);
+        }
+
+        data[i] = pal_id << 12 | tile_id;
+    }
 }
 
 void Map::Init8bpp(const Image16Bpp& image16)
--- cli/reductionhelper.hpp
+++ cli/reductionhelper.hpp
@@ -11,12 +11,18 @@
 
 #include <Magick++.h>
 
-#include "tile.hpp"
-
+#define TILE_SIZE 64
+#define PALETTE_SIZE 16
+#define TILE_SIZE_BYTES_8BPP 64
+#define TILE_SIZE_BYTES_4BPP 32
+#define TILE_SIZE_SHORTS_8BPP 32
+#define TILE_SIZE_SHORTS_4BPP 16
 #define TOTAL_TILE_MEMORY_BYTES 65536
 #define SIZE_CBB_BYTES (8192 * 2)
 #define SIZE_SBB_BYTES (1024 * 2)
 #define SIZE_SBB_SHORTS 1024
+
+#include "color.hpp"
 
 class Image16Bpp
 {
@@ -36,7 +42,9 @@ class Image16Bpp
 class Palette
 {
     public:
+        Palette() {}
         Palette(const std::vector<Color>& _colors, const std::string& _name);
+        void Set(const std::vector<Color>& _colors);
         Color At(int index) const {return colors[index];}
         int Search(const Color& color) const;
         int Search(unsigned short color) const;
@@ -76,13 +84,65 @@ class Image8BppScene
         std::shared_ptr<Palette> palette;
 };
 
+class PaletteBank
+{
+    public:
+        PaletteBank(const std::vector<Color>& _colors, int id);
+        bool Contains(const Palette& palette) const;
+        int CanMerge(const Palette& palette) const;
+        void Merge(const Palette& palette);
+        unsigned int Size() const {return colors.size();};
+        int Search(const Color& c) const;
+        void WriteData(std::ostream& file) const;
+        void WriteExport(std::ostream& file) const;
+        std::vector<Color> colors;
+        std::set<Color> colorSet;
+        int id;
+};
+
+template <class T>
+class Tile
+{
+    public:
+        typedef Tile<unsigned char> GBATile;
+        typedef Tile<unsigned short> ImageTile;
+
+        Tile();
+        Tile(const Tile<T>& tile);
+        Tile(const std::vector<T>& image, int pitch, int tilex, int tiley, int border = 0, int bpp = 8);
+        Tile(const T* image, int pitch, int tilex, int tiley, int border = 0, int bpp = 8);
+        Tile(std::shared_ptr<ImageTile> imageTile, int bpp);
+        ~Tile() {};
+        void Set(const std::vector<T>& image, int pitch, int tilex, int tiley, int border = 0, int bpp = 8);
+        void Set(const T* image, int pitch, int tilex, int tiley, int border = 0, int bpp = 8);
+        void UsePalette(const PaletteBank& bank);
+        bool IsEqual(const Tile<T>& other) const;
+        bool IsSameAs(const Tile<T>& other) const;
+        bool operator<(const Tile<T>& other) const;
+        bool operator==(const Tile<T>& other) const;
+        int id;
+        std::vector<T> data;
+        int bpp;
+        unsigned char palette_bank;
+        Palette palette;
+        std::shared_ptr<ImageTile> sourceTile;
+
+    template <class U>
+    friend std::ostream& operator<<(std::ostream& file, const Tile<U>& tile);
+};
+
+typedef Tile<unsigned char> GBATile;
+typedef Tile<unsigned short> ImageTile;
+
 class Tileset
 {
     public:
         Tileset(const std::vector<Image16Bpp>& images, const std::string& name, int bpp);
         Tileset(const Image16Bpp& image, int bpp);
-        int Search (const GBATile& tile) const;
-        int Search (const ImageTile& tile) const;
+        int Search(const GBATile& tile) const;
+        int Search(const ImageTile& tile) const;
+        // Match Imagetile to GBATile (only for bpp = 4)
+        bool Match(const ImageTile& tile, int& tile_id, int& pal_id) const;
         unsigned int Size() const {return tiles.size() * ((bpp == 4) ? TILE_SIZE_SHORTS_4BPP : (bpp == 8) ? TILE_SIZE_SHORTS_8BPP : 1);};
         void WriteData(std::ostream& file) const;
         void WriteExport(std::ostream& file) const;
@@ -91,8 +151,11 @@ class Tileset
         // Only one of two will be used bpp = 4 or 8: tiles 16: itiles
         std::set<GBATile> tiles;
         std::set<ImageTile> itiles;
+        // Bookkeeping matcher used when bpp = 4
+        std::map<ImageTile, GBATile> matcher;
         // Only one max will be used bpp = 4: paletteBanks 8: palette 16: neither
         std::shared_ptr<Palette> palette;
+        std::vector<PaletteBank> paletteBanks;
         // Only valid for bpp = 4 and 8
         std::vector<int> offsets;
     private:
@@ -133,5 +196,121 @@ class MapScene
         std::vector<Map> maps;
         std::shared_ptr<Tileset> tileset;
 };
+
+template <class T>
+Tile<T>::Tile() : id(0), data(TILE_SIZE), bpp(8), palette_bank(0)
+{
+}
+
+template <class T>
+Tile<T>::Tile(const Tile<T>& tile) : id(tile.id), data(tile.data), bpp(tile.bpp), palette_bank(tile.palette_bank), palette(tile.palette), sourceTile(tile.sourceTile)
+{
+}
+
+template <class T>
+Tile<T>::Tile(std::shared_ptr<ImageTile> imageTile, int bpp)
+{
+    printf("Not implemented");
+}
+
+template <class T>
+Tile<T>::Tile(const std::vector<T>& image, int pitch, int tilex, int tiley, int border, int bpp) : data(TILE_SIZE)
+{
+    Set(image, pitch, tilex, tiley, border, bpp);
+}
+
+template <class T>
+Tile<T>::Tile(const T* image, int pitch, int tilex, int tiley, int border, int bpp) : data(TILE_SIZE)
+{
+    Set(image, pitch, tilex, tiley, border, bpp);
+}
+
+template <class T>
+void Tile<T>::Set(const std::vector<T>& image, int pitch, int tilex, int tiley, int border, int bpp)
+{
+    this->bpp = bpp;
+    T* ptr = data.data();
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            ptr[i * 8 + j] = image[(tiley * (8+border) + i) * pitch + tilex * (8+border) + j];
+        }
+    }
+}
+
+template <class T>
+void Tile<T>::Set(const T* image, int pitch, int tilex, int tiley, int border, int bpp)
+{
+    this->bpp = bpp;
+    T* ptr = data.data();
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            ptr[i * 8 + j] = image[(tiley * (8+border) + i) * pitch + tilex * (8+border) + j];
+        }
+    }
+}
+
+template <class T>
+void Tile<T>::UsePalette(const PaletteBank& bank)
+{
+    printf("Not implemented");
+}
+
+template <class T>
+bool Tile<T>::IsEqual(const Tile<T>& other) const
+{
+    return data == other.data;
+}
+
+template <class T>
+bool Tile<T>::operator==(const Tile<T>& other) const
+{
+    return IsEqual(other);
+}
+
+template <class T>
+bool Tile<T>::operator<(const Tile<T>& other) const
+{
+    return id < other.id;
+}
+
+template <class T>
+bool Tile<T>::IsSameAs(const Tile<T>& other) const
+{
+    bool same, sameh, samev, samevh;
+    same = sameh = samev = samevh = true;
+    for (int i = 0; i < TILE_SIZE; i++)
+    {
+        int x = i % 8;
+        int y = i / 8;
+        same = same && data[i] == other.data[i];
+        samev = samev && data[i] == other.data[(7 - y) * 8 + x];
+        sameh = sameh && data[i] == other.data[y * 8 + (7 - x)];
+        samevh = samevh && data[i] == other.data[(7 - y) * 8 + (7 - x)];
+    }
+    return same || samev || sameh || samevh;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& file, const Tile<T>& tile)
+{
+
+    file << std::hex;
+    for (unsigned int i = 0; i < 8; i++)
+    {
+        for (unsigned int j = 0; j < 8; j++)
+            file << tile.data[i * 8 + j] << " ";
+        file << std::endl;
+    }
+    file << std::dec;
+    return file;
+}
+
+bool TilesPaletteSizeComp(const GBATile& i, const GBATile& j);
 
 #endif
